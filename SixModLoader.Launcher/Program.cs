9e737b4eed32aff2f73c823e7a63c4ace4bb40ae
Jakub Staroń <kubastaron@hotmail.com>
2020-08-06 21:25:51 +0200

    System.CommandLine, yay

--- SixModLoader.Launcher/Program.cs
+++ SixModLoader.Launcher/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -9,6 +12,7 @@ using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using NuGet.Versioning;
 using SixModLoader.Launcher.Doorstop;
+using Process = System.Diagnostics.Process;
 
 namespace SixModLoader.Launcher
 {
@@ -16,52 +20,91 @@ namespace SixModLoader.Launcher
     {
         public const string SixModLoaderPath = "SixModLoader/bin/SixModLoader.dll";
 
-        public static HttpClient HttpClient { get; } = new HttpClient();
         public static SemanticVersion Version { get; } = SemanticVersion.Parse(Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion);
+        public static HttpClient HttpClient { get; } = new HttpClient();
+        public static IDoorstop Doorstop { get; private set; }
 
-        public static async Task Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
             HttpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("SixModLoader.Launcher", Version.ToString()));
             Console.WriteLine($"SixModLoader.Launcher {Version}");
 
-            await new AutoUpdate().UpdateAsync();
-
-            string defaultProcess;
-            IDoorstop doorstop;
-
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
-                defaultProcess = "LocalAdmin";
-                doorstop = new LinuxDoorstop();
+                Doorstop = new LinuxDoorstop();
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                defaultProcess = "LocalAdmin.exe";
-                doorstop = new WindowsDoorstop();
+                Doorstop = new WindowsDoorstop();
             }
             else
             {
                 throw new InvalidOperationException("Unsupported OS");
             }
 
-            await doorstop.DownloadAsync();
-            
+            const string jumploaderFile = "jumploader.txt";
+
+            if (File.Exists(jumploaderFile))
+            {
+                args = CommandLineStringSplitter.Instance.Split(File.ReadAllText(jumploaderFile).Replace("{args}", string.Join(" ", args))).ToArray();
+            }
+
+            var updateCommand = new Command("update", "Updates launcher and SixModLoader")
+            {
+                Handler = CommandHandler.Create(UpdateAsync)
+            };
+
+            var launchCommand = new Command("launch", "Runs update command and launches game")
+            {
+                new Argument<string>("args", () =>
+                {
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    {
+                        return "LocalAdmin";
+                    }
+
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    {
+                        return "LocalAdmin.exe";
+                    }
+
+                    throw new InvalidOperationException("Unsupported OS");
+                })
+            };
+
+            launchCommand.Handler = CommandHandler.Create<string>(args => LaunchAsync(CommandLineStringSplitter.Instance.Split(args).ToArray()));
+
+            var rootCommand = new RootCommand
+            {
+                updateCommand,
+                launchCommand
+            };
+
+            return await rootCommand.InvokeAsync(args);
+        }
+
+        public static async Task UpdateAsync()
+        {
+            await new AutoUpdate().UpdateAsync();
+            await Doorstop.DownloadAsync();
+        }
+
+        public static async Task LaunchAsync(string[] args)
+        {
+            await UpdateAsync();
+
             Console.WriteLine();
 
-            var file = (File.Exists("jumploader.txt") ? File.ReadAllText("jumploader.txt") : defaultProcess + " {args}")
-                .Replace("{args}", string.Join(" ", args))
-                .Split(' ');
+            Console.WriteLine($"Starting \"{string.Join(" ", args).Trim()}\"");
 
-            Console.WriteLine($"Starting \"{string.Join(" ", file).Trim()}\"");
-
-            doorstop.PreLaunch();
+            Doorstop.PreLaunch();
 
             var process = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
-                    FileName = file.First(),
-                    Arguments = string.Join(" ", file.Skip(1)),
+                    FileName = args.First(),
+                    Arguments = string.Join(" ", args.Skip(1)),
                     UseShellExecute = false,
                     CreateNoWindow = false
                 }
